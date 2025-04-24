5c978b468c1b8d54fac61a31fc0dd794a9b4b471
Teo_You <teoyou881@gmail.com>
2025-04-24 19:20:25 -0400

    router.replace doesn't work -> infinite get call error occurs.
    use useEffect hook.

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -3,12 +3,33 @@
 // 서버 컴포넌트와 클라이언트 컴포넌트가 redirect를 처리하는 방식이 다르다.
 import {useRouter} from 'next/navigation';
 import {useEffect} from 'react';
-import Main from "@/app/(beforeLogin)/_component/Main";
+import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Login() {
   const router = useRouter();
-  router.replace('/i/flow/login');
-  return <Main />;
+
+  /*
+  * useEffect 훅을 사용하는 이유
+
+  초기 렌더링 후 실행: useEffect 훅은 컴포넌트가 처음 렌더링된 후에 실행됩니다. 이는 리디렉션과 같은 작업을 수행하기에 적절한 시점입니다.
+
+  상태 업데이트 후 실행: useEffect는 특정 상태 변수가 변경될 때에도 실행될 수 있습니다. 이는 상태 변화에 따라 리디렉션을 수행해야 하는 경우에 유용합니다.
+
+  비동기 작업 처리: useEffect는 비동기 작업을 처리하는 데 적합합니다. 라우팅 작업이 비동기적으로 처리될 수 있으므로, useEffect 내에서 이를 수행하는 것이 안전합니다.
+
+  useEffect 훅 없이 router.replace를 사용하는 경우의 문제점
+
+  컴포넌트 함수 내에서 직접 router.replace를 호출하면 다음과 같은 문제가 발생할 수 있습니다.
+
+  렌더링 중 상태 변경: React는 컴포넌트가 렌더링되는 동안에는 상태를 변경하는 것을 허용하지 않습니다. router.replace는 내부적으로 상태를 변경할 수 있으므로, 렌더링 과정에서 호출하면 오류가 발생할 수 있습니다.
+
+  예측 불가능한 동작: 컴포넌트 함수는 여러 번 호출될 수 있으며, 이로 인해 router.replace가 의도치 않게 여러 번 호출될 수 있습니다. 이는 예기치 않은 동작을 초래할 수 있습니다.
+  * */
+  useEffect(() => {
+    router.replace('/i/flow/login');
+  }, [router]);
+
+  return <Main/>;
 
   // redirect('/i/flow/login');
 }
