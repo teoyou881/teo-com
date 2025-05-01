e2509c2a07b722acdd90b77de475b2f85442e798
Teo_You <teoyou881@gmail.com>
2025-05-01 02:21:16 -0400

    create RightSearchZone & SearchForm

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import React from 'react';
 import style from './trendSection.module.css'
 import Trend from '@/app/(afterLogin)/_component/Trend';
+import { usePathname } from 'next/navigation';
 
 const TrendSection = () => {
+
+  const pathname = usePathname();
+if(pathname === '/explore'){
+  return null;
+}
   return (
       <div className={style.trendBg}>
         <div className={style.trend}>
--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,19 +1,20 @@
-import React, {ReactNode} from "react";
+import FollowRecommend from '@/app/(afterLogin)/_component/FollowRecommend';
+import LogoutButton from '@/app/(afterLogin)/_component/LogoutButton';
+import NavMenu from '@/app/(afterLogin)/_component/NavMenu';
+import TrendSection from '@/app/(afterLogin)/_component/TrendSection';
 import style from '@/app/(afterLogin)/layout.module.css';
-import Link from "next/link";
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
+import React, {ReactNode} from 'react';
 import TLogo from '../../../public/tLogo.png';
-import NavMenu from "@/app/(afterLogin)/_component/NavMenu";
-import LogoutButton from "@/app/(afterLogin)/_component/LogoutButton";
-import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
-import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
+import RightSearchZone from './_component/RightSearchZone';
 
 type Props = {
   children: ReactNode;
   modal: ReactNode;
 };
 
-export default function AfterLoginLayout({ children, modal }: Props) {
+export default function AfterLoginLayout({children, modal}: Props) {
   return (
       <div className={style.container}>
         <header className={style.leftSectionWrapper}>
@@ -21,16 +22,17 @@ export default function AfterLoginLayout({ children, modal }: Props) {
             <div className={style.leftSectionFixed}>
               <Link className={style.logo} href="/home">
                 <div className={style.logoPill}>
-                  <Image src={TLogo} alt="z.com로고" width={40} height={40} />
+                  <Image src={TLogo} alt="z.com로고" width={40} height={40}/>
                 </div>
               </Link>
               <nav>
                 <ul>
-                  <NavMenu />
+                  <NavMenu/>
                 </ul>
-                <Link href=" /compose/tweet" className={style.postButton}>게시하기</Link>
+                <Link href=" /compose/tweet"
+                      className={style.postButton}>게시하기</Link>
               </nav>
-              <LogoutButton />
+              <LogoutButton/>
             </div>
           </section>
         </header>
@@ -38,23 +40,13 @@ export default function AfterLoginLayout({ children, modal }: Props) {
           <div className={style.rightSectionInner}>
             <main className={style.main}>{children}</main>
             <section className={style.rightSection}>
-              <div style={{ marginBottom: 60, width: 'inherit' }}>
-                <form className={style.search}>
-                  <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
-                    <g>
-                      <path
-                          d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-                    </g>
-                  </svg>
-                  <input type="search" />
-                </form>
-              </div>
-              <TrendSection />
+              <RightSearchZone/>
+              <TrendSection/>
               <div className={style.followRecommend}>
                 <h3>Who to follow</h3>
-                <FollowRecommend />
-                <FollowRecommend />
-                <FollowRecommend />
+                <FollowRecommend/>
+                <FollowRecommend/>
+                <FollowRecommend/>
               </div>
             </section>
           </div>
