206662880bb2e52ba3442845b0bec63a61c0b73e
Teo_You <teoyou881@gmail.com>
2025-05-01 01:47:51 -0400

    create /compose/tweet I made it so that the modal window opens when you hit refresh using useeffect

--- src/app/(afterLogin)/compose/tweet/page.tsx
+++ src/app/(afterLogin)/compose/tweet/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import Home from "@/app/(afterLogin)/home/page";
+import TweetModal from "@/app/(afterLogin)/@modal/(.)compose/tweet/page"; // 가상의 모달 컴포넌트
+import { useEffect, useState } from "react";
+
+
+export default function Page() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    setIsModalOpen(true); // 페이지 로드시 모달을 표시하도록 상태 설정
+  }, []);
+
   return (
-
-    <>compose tweet</>
+      <>
+        <Home />
+        {isModalOpen && <TweetModal />}
+      </>
   );
 }
