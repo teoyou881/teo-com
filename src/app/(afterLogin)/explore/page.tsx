c1ec3f375913ebc94f9e28bf6b2dd1074177d09c
Teo_You <teoyou881@gmail.com>
2025-05-01 14:05:20 -0400

    create explore page

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import Trend from '@/app/(afterLogin)/_component/Trend';
+import SearchForm from '../_component/SearchForm';
+import style from './explore.module.css'
 
 export default function Home() {
   return (
-    <>explore</>
+      <main className={style.main}>
+        <div className={style.formZone}>
+          <SearchForm/>
+        </div>
+        <div className="style.trned">
+          <h3>You might like</h3>
+          <Trend/>
+          <Trend/>
+          <Trend/>
+        </div>
+      </main>
   );
 }
