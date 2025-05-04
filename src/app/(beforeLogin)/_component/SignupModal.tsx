05342f92a8b3882e58e24202b09a3693e1c033ca
Teo_You <teoyou881@gmail.com>
2025-05-03 22:24:01 -0400

    even though I try to send from to right url, it doestn't work. I don't know why.

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,49 +1,71 @@
-"use client";
-
+import {redirect} from 'next/navigation';
+import BackButton from './BackButton';
 import style from './signup.module.css';
-import {useRouter} from "next/navigation";
-import {ChangeEventHandler, FormEventHandler, useState} from "react";
+import Form from 'next/form';
 
-export default function SignupModal() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [image, setImage] = useState('');
-  const [imageFile, setImageFile] = useState<File>();
+function validateFormData(formData: FormData) {
+  // 필수 필드 목록과 관련 오류 메시지 정의
+  const requiredFields = [
+    {name:'id', message:'아이디를 입력해주세요.'},
+    {name:'name', message:'이름을 입력해주세요.'},
+    {name:'password', message:'비밀번호를 입력해주세요.'},
+    {name:'image', message:'이미지를 업로드해주세요.'},
+  ];
 
-  const router = useRouter();
-  const onClickClose = () => {
-    router.back();
-    // TODO: 뒤로가기가 /home이 아니면 /home으로 보내기
+  // 누락된 필드 찾기
+  const missingFields = requiredFields.filter(
+      field => !formData.get(field.name));
+
+  // 누락된 필드가 있으면 첫 번째 오류 메시지 반환
+  if (missingFields.length > 0) {
+    return {success:false, message:missingFields[0].message};
   }
 
-  const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => { setId(e.target.value) };
+  // 추가 유효성 검사 (예: 비밀번호 형식 등)
+  const password = formData.get('password') as string;
+  if (password.length < 8) {
+    return {success:false, message:'비밀번호는 8자 이상이어야 합니다.'};
+  }
 
-  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => { setPassword(e.target.value) };
-  const onChangeNickname: ChangeEventHandler<HTMLInputElement> = (e) => { setNickname(e.target.value) };
-  const onChangeImageFile: ChangeEventHandler<HTMLInputElement> = (e) => {
-    e.target.files && setImageFile(e.target.files[0])
-  };
+  // 모든 유효성 검사 통과
+  return {success:true};
+}
 
-  const onSubmit: FormEventHandler = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:9090/api/users', {
-      method: 'post',
-      body: JSON.stringify({
-        id,
-        nickname,
-        image,
-        password,
-      }),
-      credentials: 'include',
-    }).then((response: Response) => {
+export default function SignupModal() {
+
+  const submit = async (formData: FormData) => {
+    'use server';
+
+    const data = validateFormData(formData);
+    if (data.success === false) {
+      return data.message;
+    }
+
+    let shouldRedirect = false;
+    try {
+      const response = await fetch(
+          `http://localhost:3000/api/users`, {
+            method     :'POST',
+            body       :formData,
+            credentials:'include',
+          });
       console.log(response.status);
-      if (response.status === 200) {
-        router.replace('/home');
+      if (response.status === 403) {
+        console.log('user_exists');
+        return {message:'user_exists'}
       }
-    }).catch((err) => {
-      console.error(err);
-    });
+      if (response.status === 404) {
+        console.log('not_found');
+        return {message:'not_found'}
+      }
+      shouldRedirect = true;
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+    if (shouldRedirect) {
+      redirect('/home')
+    }
   }
 
   return (
@@ -51,52 +73,50 @@ export default function SignupModal() {
         <div className={style.modalBackground}>
           <div className={style.modal}>
             <div className={style.modalHeader}>
-              <button className={style.closeButton} onClick={onClickClose}>
-                <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
-                     className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
-                  <g>
-                    <path
-                        d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                  </g>
-                </svg>
-              </button>
-              <div>계정을 생성하세요.</div>
+              <BackButton/>
+              <div>Create your account</div>
             </div>
-            <form>
+            <Form action={submit}>
               <div className={style.modalBody}>
                 <div className={style.inputDiv}>
-                  <label className={style.inputLabel} htmlFor="id">아이디</label>
-                  <input id="id" className={style.input} type="text" placeholder=""
-                         value={id}
-                         onChange={onChangeId}
+                  <label className={style.inputLabel} htmlFor="id">Id</label>
+                  <input id="id" name="id" className={style.input} type="text"
+                         placeholder=""
+                         required
                   />
                 </div>
                 <div className={style.inputDiv}>
-                  <label className={style.inputLabel} htmlFor="name">닉네임</label>
-                  <input id="name" className={style.input} type="text" placeholder=""
-                         value={nickname}
-                         onChange={onChangeNickname}
+                  <label className={style.inputLabel}
+                         htmlFor="name">Nickname</label>
+                  <input id="name" name="name" className={style.input}
+                         type="text"
+                         placeholder=""
+                         required
                   />
                 </div>
                 <div className={style.inputDiv}>
-                  <label className={style.inputLabel} htmlFor="password">비밀번호</label>
-                  <input id="password" className={style.input} type="password" placeholder=""
-                         value={password}
-                         onChange={onChangePassword}
+                  <label className={style.inputLabel}
+                         htmlFor="password">Password</label>
+                  <input id="password" name="password" className={style.input}
+                         type="password" placeholder=""
+                         required
                   />
                 </div>
                 <div className={style.inputDiv}>
-                  <label className={style.inputLabel} htmlFor="image">프로필</label>
-                  <input id="image" className={style.input} type="file" accept="image/*"
-                         onChange={onChangeImageFile}
+                  <label className={style.inputLabel}
+                         htmlFor="image">Profile</label>
+                  <input id="image" name="image" className={style.input}
+                         type="file" accept="image/*"
+                         required
                   />
                 </div>
               </div>
               <div className={style.modalFooter}>
-                <button className={style.actionButton} disabled>가입하기</button>
+                <button className={style.actionButton}>Register</button>
               </div>
-            </form>
+            </Form>
           </div>
         </div>
-      </>)
+      </>
+  )
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,30 +10,33 @@ const User = [
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const handlers = [
   http.post(`${baseUrl}/api/login`, () => {
-    console.log('로그인');
+    console.log('Sign in');
     return HttpResponse.json(User[1], {
       headers: {
-        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+        'Content-Type': 'application/json; charset=UTF-8'
+
       },
     })
   }),
   http.post(`${baseUrl}/api/logout`, () => {
-    console.log('로그아웃');
+    console.log('Logout');
     return new HttpResponse(null, {
       headers: {
         'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0'
       }
     })
   }),
-  http.post(`${baseUrl}/api/users`, async ({ request }) => {
-    console.log('회원가입');
-    // return HttpResponse.text(JSON.stringify('user_exists'), {
-    //   status: 403,
-    // });
-    return HttpResponse.text(JSON.stringify('ok'), {
-      headers: {
-        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
-      },
+  http.post(`http://localhost:3000/api/users`, async ({ request }) => {
+    console.log('Sign up');
+    return HttpResponse.text(JSON.stringify('user_exists'), {
+      status: 403,
     });
+    // return HttpResponse.text(JSON.stringify('ok'), {
+    //   headers: {
+    //     'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
+    //     'Content-Type': 'application/json; charset=UTF-8'
+    //   },
+    // });
   })
 ];
